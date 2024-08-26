39c66669a65df57e15f2dafbd7ede2f5b66e752b
Ebrahim Elghrbawy <HIMA132004@GMAIL.COM>
2024-08-26 08:09:23 +0300

    delete temp projects

--- tmp/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace tmp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
